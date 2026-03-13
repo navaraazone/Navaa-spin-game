3957f871851de44e25374e26d850526e316f78a6
NAVARAA ZONE <navaraa.zone@gmail.com>
2026-03-13 20:51:11 +0530

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,68 @@
-let points = 0;
+let canvas = document.getElementById("wheel");
+let ctx = canvas.getContext("2d");
+
+let rewards=[0,5,10,20,50,0,15,30];
+
+let startAngle=0;
+let arc=Math.PI/(rewards.length/2);
+
+let points=0;
+
+function drawWheel(){
+
+for(let i=0;i<rewards.length;i++){
+
+let angle=startAngle+i*arc;
+
+ctx.fillStyle=i%2==0?"#ffcc00":"#ff6600";
+
+ctx.beginPath();
+
+ctx.arc(150,150,150,angle,angle+arc,false);
+
+ctx.arc(150,150,0,angle+arc,angle,true);
+
+ctx.fill();
+
+ctx.save();
+
+ctx.fillStyle="black";
+
+ctx.translate(
+150+Math.cos(angle+arc/2)*100,
+150+Math.sin(angle+arc/2)*100
+);
+
+ctx.fillText(rewards[i],-10,0);
+
+ctx.restore();
+
+}
+
+}
+
+drawWheel();
 
 function spin(){
 
-let rewards=[0,5,10,20,50];
+let spinAngle=Math.floor(Math.random()*360)+720;
 
-let result=rewards[Math.floor(Math.random()*rewards.length)];
+startAngle+=spinAngle*Math.PI/180;
 
-points += result;
+ctx.clearRect(0,0,300,300);
 
-document.getElementById("result").innerHTML =
-"Reward: "+result+" NAVAA";
+drawWheel();
 
-document.getElementById("points").innerHTML =
+let index=Math.floor(Math.random()*rewards.length);
+
+let reward=rewards[index];
+
+points+=reward;
+
+document.getElementById("result").innerHTML=
+"Reward: "+reward+" NAVAA";
+
+document.getElementById("points").innerHTML=
 "Points: "+points;
 
 }
